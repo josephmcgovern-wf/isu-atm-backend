9a63ddb8a6ab83065828273068fd65a59562c5ec
Joseph McGovern <joseph.mcgovern@webfilings.com>
2016-12-01 21:06:06 -0600

    derp

--- local_setup.py
+++
@@ -1,6 +0,0 @@
-from src.user import User
-
-User.add('user@example.com', 'password')
-print "Added user to database with the following credentials"
-print "Email: user@example.com"
-print "Password: password"
